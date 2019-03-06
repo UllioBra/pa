6f83b4604f759679fde19094d5093a0cf62e3ce7
UllioBra <3131367211@qq.com>
2019-03-06 16:26:31 +0800

    3.6.2019

--- book/linovel/Main.py
+++ book/linovel/Main.py
@@ -1,13 +1,8 @@
 import sys
 import pa
-
-sys.path.append('../epublib')
-import gsx
+import epublib.epublib.EpubBook as E
 
 ep_url = 'F:/Autobook/epub/'
 bs_path, lis = pa.to()
 for i in lis:
-    inp = bs_path + i['name'] + '/'
-    gsx.init_epub(ep_url, i['name'], i['author'])
-    gsx.update_epub(ep_url + i['name'], inp)
-    gsx.make_epub(ep_url, i['name'], 'E:/book/made by ulo/')
+    k = E()
